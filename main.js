2b035dc76d591fe356cfc8f7748170b250852459
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-03-19 01:19:28 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var ShowSum = function(){
+
+    var num1 = document.getElementsByTagName('input')[0].value;
+
+    var num2 = document.getElementsByTagName('input')[1].value;
+
+    var result = parseInt(num2) / parseInt(num1);
+
+    document.getElementsByTagName("input")[2].value = result.toFixed(4) * 100 + "%";
+}
